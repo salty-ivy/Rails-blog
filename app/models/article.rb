91fa6adec7db80ec4ff1a8c296ca7decaa454cf8
4the4ryushin <aman2001mi@gmail.com>
2022-04-16 04:04:33 +0530

    model added

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 3,maximum:60}
+    validates :description, presence: true, length: {minimum:3, maximum:600}
+end
